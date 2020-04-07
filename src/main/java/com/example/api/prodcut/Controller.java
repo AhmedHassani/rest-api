cf06a87d95351d7909f2366950bf7aac37da454a
Ahmed Hassani <ah.jr22@gmail.com>
2020-04-08 01:05:33 +0300

    first

---
+++ src/main/java/com/example/api/prodcut/Controller.java
@@ -0,0 +1,16 @@
+package com.example.api.prodcut;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+     @GetMapping(value = {"/",""})
+     public String Hello(){
+             return "Test REST API";
+     }
+
+
+}
